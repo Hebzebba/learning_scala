c92179077e0fb5e72bf4d48e87948409359a3c11
Hebzebba <kwashieseth34@gmail.com>
2021-02-20 21:54:16 +0000

    Introduction and variables

---
+++ Introduction/Introduction.scala
@@ -0,0 +1,7 @@
+object Introduction{
+
+def main(args:Array[String]){
+    println("Hello world this is my first scala project")
+    }
+
+}
---
+++ variables/Variables.scala
@@ -0,0 +1,22 @@
+object Variables{
+    def main(args:Array[String]){
+
+        //This type of variable declaration is mutable.
+        var myVar : String = "Seth";
+
+        //This type of variable declaration in immutable.
+        val myVal : String = "Gregory";
+
+        println(myVar + " " + myVal );
+
+        myVar = "Yaw";
+        println(myVar + " " + myVal );
+
+
+        //multiple assignment
+        val (myVar1,myVar2) = Pair(40,"Foo");
+
+        println(myVar1);
+
+    }
+}
